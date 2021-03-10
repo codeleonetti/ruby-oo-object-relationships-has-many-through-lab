6bb921c6779961d29fae835a3c67b76881dd02ab
john <john.d.leonetti@gmail.com>
2021-03-09 21:48:23 -0800

    done

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,30 @@
+class Doctor
+
+    @@all = []
+
+    attr_accessor :name
+    #attr_reader
+
+    def initialize(name)
+        @name = name
+        @@all << self
+
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, name)
+        Appointment.new(date,name, self)
+    end
+
+
+
+
+
+
+
+end
+
 
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,39 @@
+require "pry"
 
+class Patient
+
+    @@all = []
+
+    attr_accessor :name
+    attr_reader
+
+    def initialize(name)
+        @name = name
+        @@all << self
+
+
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doc)
+       # binding.pry
+     # Appointment = date.Doctor
+     appointment = date && doc
+
+
+        # need a date and doc with patient
+    end
+
+    def appointments
+
+    end
+
+    def doctors
+
+    end
+
+
+end
